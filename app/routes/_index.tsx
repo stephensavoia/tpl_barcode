bf81f7926ecfa4485641f4ef8f7364a29be9b05e
stephensavoia <ssavoia@gmail.com>
2024-09-11 01:34:19 -0400

    UI updates

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,23 @@
 import "flowbite";
-import type { LinksFunction, MetaFunction } from "@remix-run/cloudflare";
-import type { CarouselProps, CustomFlowbiteTheme } from "flowbite-react";
+import type {
+  ActionFunctionArgs,
+  LinksFunction,
+  MetaFunction,
+} from "@remix-run/cloudflare";
+import type { CustomFlowbiteTheme } from "flowbite-react";
 import {
   Flowbite,
   Card,
   Button,
   TextInput,
   Label,
-  Select,
   Carousel,
+  Alert,
 } from "flowbite-react";
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Form, Link, useActionData } from "@remix-run/react";
+import { HiInformationCircle } from "react-icons/hi";
+import AspectRatioSelector from "~/components/AspectRatioSelector";
 
 export const meta: MetaFunction = () => {
   return [
@@ -64,157 +71,233 @@ const customTheme: CustomFlowbiteTheme = {
   },
 };
 
+export async function action({ request }: ActionFunctionArgs) {
+  let formData = await request.formData();
+
+  console.log(formData);
+
+  let cardNumber = String(formData.get("cardNumber") || "");
+  let design = String(formData.get("design") || "0");
+
+  let errors: { cardNumber?: string } = {};
+
+  switch (true) {
+    case !cardNumber:
+      errors.cardNumber = "Card number is required.";
+      return { errors };
+    case cardNumber.length !== 14:
+      errors.cardNumber = "Card number must be exactly 14 digits.";
+      return { errors };
+    case /\D/.test(cardNumber):
+      errors.cardNumber = "Card number must contain only digits.";
+      return { errors };
+    default:
+      break;
+  }
+
+  return String(cardNumber) + " " + String(design);
+
+  //   let errors = await validate(email, password);
+  //   if (errors) {
+  //     return json({ ok: false, errors }, 400);
+  //   }
+}
+
+// Had to add this because typescript is too stupid to understand what a "?" means
+type ActionResult = {
+  errors?: {
+    cardNumber?: string;
+  };
+};
+
 export default function Index() {
-  const cardNumberRef = useRef<HTMLInputElement>(null);
-  // const aspectRatioRef = useRef<HTMLSelectElement>(null);
+  const actionResult = useActionData<ActionResult>();
+  const [design, setDesign] = useState(0);
+  const [cardNumberError, setCardNumberError] = useState(false);
   const radioRefs = useRef<HTMLInputElement[]>([]);
 
-  const handleCarouselChange = (index: number) => {
+  useEffect(() => {
+    if (actionResult && actionResult.errors?.cardNumber) {
+      setCardNumberError(true);
+    }
+  }, [actionResult]);
+
+  const handleCardNumberChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const cardNumber = event.target.value;
+      console.log(cardNumber);
+      console.log(cardNumber.length !== 14);
+      console.log(/\D/.test(cardNumber));
+      if (cardNumber.length == 14 && /^\d+$/.test(cardNumber)) {
+        setCardNumberError(false);
+        console.log("Card number is valid");
+      }
+    },
+    []
+  );
+
+  const handleCarouselChange = useCallback((index: number) => {
     if (radioRefs.current[index]) {
       radioRefs.current[index].checked = true;
     }
-  };
-
-  const handleFormSubmit = () => {
-    let cardNumber = cardNumberRef.current?.value;
-    // let aspectRatio = aspectRatioRef.current?.value;
-    let selectedRadio = radioRefs.current.find((radio) => radio.checked);
-    alert(
-      "This app isn't quite finished yet.\nPlease come back soon!" +
-        "\n\nCard Number: " +
-        String(cardNumber) +
-        // "\nPhone Size: " +
-        // String(aspectRatio) +
-        "\nWallpaper: " +
-        selectedRadio?.value
-    );
-    radioRefs.current[0].click();
-    // relaod page
-    location.reload();
-  };
-
-  useEffect(() => {
-    if (radioRefs.current[0]) {
-      console.log(radioRefs.current[0].value);
-    }
   }, []);
+
   return (
     <Flowbite theme={{ theme: customTheme }}>
       <div className="container mx-auto max-w-md p-5 overflow-hidden">
         <h1 className="max-w-md">TPL Barcode Generator</h1>
-        <Card className="max-w-md mb-8">
-          <div className="flex max-w-md flex-col gap-4">
-            <div>
-              <div className="mb-2 block">
-                <Label
-                  htmlFor="cardNumber"
-                  // color="failure"
-                  value="Library card number (14 digits)"
-                />
-              </div>
-              <TextInput
-                ref={cardNumberRef}
-                id="cardNumber"
-                required
-                // color="failure"
-                // helperText={
-                //   <>
-                //     <span className="text-md">Oops!</span> Username already
-                //     taken!
-                //   </>
-                // }
-              />
-            </div>
-          </div>
-
-          {/* <div className="max-w-md">
-            <div className="mb-2 block">
-              <Label htmlFor="aspectRatio" value="Phone display aspect ratio" />
-            </div>
-            <Select ref={aspectRatioRef} id="aspectRatio" required>
-              <option value="0">9:19.5 (iPhone 11 and newer)</option>
-              <option value="1">9:16 (iPhone SE to 8+)</option>
-              <option value="2">3:4 (iPhone 4s and older)</option>
-            </Select>
-          </div> */}
-
-          <div className="block">
-            <span className="text-sm font-medium text-gray-900">
-              Select wallpaper design
-            </span>
-          </div>
-
-          <div className="h-[500px]">
-            <Carousel
-              slide={false}
-              onSlideChange={(index) => handleCarouselChange(index)}
+        {actionResult && !actionResult.errors ? (
+          <Card className="max-w-md mb-8">
+            {" "}
+            <p className="text-center">
+              This app isn't quite finished yet. <br /> Please come back soon!
+              <br />
+              <br />
+              {String(actionResult)}
+            </p>
+            <Button className="mt-8" color="blue" type="submit">
+              Download Wallpaper
+            </Button>
+            <Link
+              to="/"
+              className="text-sm font-medium text-center text-blue-600 underline dark:text-blue-500 hover:no-underline"
             >
-              <div className="flex h-full items-center justify-center">
-                <label id="designRadio1" className="radio-img">
-                  <input
-                    ref={(el) => (radioRefs.current[0] = el!)}
-                    type="radio"
-                    name="design"
-                    value="0"
-                  />
-                  <div className="relative mx-auto border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[500px] w-[250px] shadow-xl">
-                    <div className="w-[148px] h-[18px] bg-gray-800 top-0 rounded-b-[1rem] left-1/2 -translate-x-1/2 -translate-y-0.5 absolute"></div>
-                    <div className="rounded-[2rem] overflow-hidden w-[222px] h-[472px] dark:bg-gray-800">
-                      <img
-                        src="http://loremflickr.com/222/472/rome"
-                        className="w-[222px] h-[472px]"
-                        alt=""
-                      />
-                    </div>
+              Generate Another Barcode
+            </Link>
+          </Card>
+        ) : (
+          <Form method="post">
+            <Card className="max-w-md mb-8">
+              <div className="flex max-w-md flex-col gap-4">
+                <div>
+                  <div className="mb-2 block">
+                    <Label
+                      htmlFor="cardNumber"
+                      color={
+                        actionResult &&
+                        actionResult.errors?.cardNumber &&
+                        cardNumberError
+                          ? "failure"
+                          : undefined
+                      }
+                      value="Library card number (14 digits)"
+                    />
                   </div>
-                </label>
-              </div>
-              <div className="flex h-full items-center justify-center">
-                <label className="radio-img">
-                  <input
-                    ref={(el) => (radioRefs.current[1] = el!)}
-                    type="radio"
-                    name="design"
-                    value="1"
+                  <TextInput
+                    id="cardNumber"
+                    name="cardNumber"
+                    required
+                    color={
+                      actionResult &&
+                      actionResult.errors?.cardNumber &&
+                      cardNumberError
+                        ? "failure"
+                        : undefined
+                    }
+                    helperText={
+                      actionResult &&
+                      actionResult.errors?.cardNumber &&
+                      cardNumberError ? (
+                        <>{actionResult.errors.cardNumber}</>
+                      ) : undefined
+                    }
+                    onChange={
+                      actionResult &&
+                      actionResult.errors?.cardNumber &&
+                      cardNumberError
+                        ? handleCardNumberChange
+                        : undefined
+                    }
                   />
-                  <div className="relative mx-auto border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[500px] w-[250px] shadow-xl">
-                    <div className="w-[148px] h-[18px] bg-gray-800 top-0 rounded-b-[1rem] left-1/2 -translate-x-1/2 -translate-y-0.5 absolute"></div>
-                    <div className="rounded-[2rem] overflow-hidden w-[222px] h-[472px] dark:bg-gray-800">
-                      <img
-                        src="http://loremflickr.com/222/472/london"
-                        className="w-[222px] h-[472px]"
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                </label>
+                </div>
               </div>
-              <div className="flex h-full items-center justify-center">
-                <label className="radio-img">
-                  <input
-                    ref={(el) => (radioRefs.current[2] = el!)}
-                    type="radio"
-                    name="design"
-                    value="2"
-                  />
-                  <div className="relative mx-auto border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[500px] w-[250px] shadow-xl">
-                    <div className="w-[148px] h-[18px] bg-gray-800 top-0 rounded-b-[1rem] left-1/2 -translate-x-1/2 -translate-y-0.5 absolute"></div>
-                    <div className="rounded-[2rem] overflow-hidden w-[222px] h-[472px] dark:bg-gray-800">
-                      <img
-                        src="http://loremflickr.com/222/472/paris"
-                        className="w-[222px] h-[472px]"
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                </label>
+
+              {/* <AspectRatioSelector /> */}
+
+              <div className="block">
+                <span className="text-sm font-medium text-gray-900">
+                  Select wallpaper design
+                </span>
               </div>
-            </Carousel>
-          </div>
-          <Button className="mt-8" color="blue" onClick={handleFormSubmit}>
-            Generate Barcode
-          </Button>
-        </Card>
+
+              <div className="h-[500px]">
+                <Carousel
+                  slide={false}
+                  onSlideChange={(index) => handleCarouselChange(index)}
+                >
+                  <div className="flex h-full items-center justify-center">
+                    <label id="designRadio1" className="radio-img">
+                      <input
+                        type="radio"
+                        name="design"
+                        value="0"
+                        ref={(el) => (radioRefs.current[0] = el!)}
+                      />
+                      <div className="relative mx-auto border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[500px] w-[250px] shadow-xl">
+                        <div className="w-[148px] h-[18px] bg-gray-800 top-0 rounded-b-[1rem] left-1/2 -translate-x-1/2 -translate-y-0.5 absolute"></div>
+                        <div className="rounded-[2rem] overflow-hidden w-[222px] h-[472px] dark:bg-gray-800">
+                          <img
+                            src="http://loremflickr.com/222/472/rome"
+                            className="w-[222px] h-[472px]"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </label>
+                  </div>
+                  <div className="flex h-full items-center justify-center">
+                    <label className="radio-img">
+                      <input
+                        type="radio"
+                        name="design"
+                        value="1"
+                        ref={(el) => (radioRefs.current[1] = el!)}
+                      />
+                      <div className="relative mx-auto border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[500px] w-[250px] shadow-xl">
+                        <div className="w-[148px] h-[18px] bg-gray-800 top-0 rounded-b-[1rem] left-1/2 -translate-x-1/2 -translate-y-0.5 absolute"></div>
+                        <div className="rounded-[2rem] overflow-hidden w-[222px] h-[472px] dark:bg-gray-800">
+                          <img
+                            src="http://loremflickr.com/222/472/london"
+                            className="w-[222px] h-[472px]"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </label>
+                  </div>
+                  <div className="flex h-full items-center justify-center">
+                    <label className="radio-img">
+                      <input
+                        type="radio"
+                        name="design"
+                        value="2"
+                        ref={(el) => (radioRefs.current[2] = el!)}
+                      />
+                      <div className="relative mx-auto border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[500px] w-[250px] shadow-xl">
+                        <div className="w-[148px] h-[18px] bg-gray-800 top-0 rounded-b-[1rem] left-1/2 -translate-x-1/2 -translate-y-0.5 absolute"></div>
+                        <div className="rounded-[2rem] overflow-hidden w-[222px] h-[472px] dark:bg-gray-800">
+                          <img
+                            src="http://loremflickr.com/222/472/paris"
+                            className="w-[222px] h-[472px]"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </label>
+                  </div>
+                </Carousel>
+              </div>
+              <Button className="mt-8" color="blue" type="submit">
+                Generate Barcode
+              </Button>
+              {actionResult && actionResult.errors && cardNumberError ? (
+                <Alert color="failure" icon={HiInformationCircle}>
+                  Oops! Check your library card number.
+                </Alert>
+              ) : null}
+            </Card>
+          </Form>
+        )}
       </div>
     </Flowbite>
   );
